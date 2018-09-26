efe799f16a1d5aaa1d2dfdec17bb8d26e51e2d53
Littlewaves666 <771652653@qq.com>
2018-09-26 17:23:40 +0800

    first commit

---
+++ zq_vue_1/src/components/Test.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="hello2">
+        <h1>{{ msg }}</h1>
+        <h2>我是测试页面</h2>
+    </div>  
+</template>
+<script>
+export default {
+  name: 'Test',
+  data () {
+    return {
+      msg: '123456'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1,h2{
+    font-weight: normal;
+    color: brown;
+}
+</style>
+
+
+
